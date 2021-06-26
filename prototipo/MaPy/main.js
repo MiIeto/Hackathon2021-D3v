5a828f9cccb56c276fe6e54ffd79cd8d6de28815
mileto_notebook <milenkotomasevic41@gmail.com>
2021-06-26 04:27:42 -0400

    cambios estilo radio + img prototipo

--- prototipo/MaPy/main.js
+++ prototipo/MaPy/main.js
@@ -4,5 +4,5 @@ var output = document.getElementById("value");
 output.innerHTML = slider.value;
 
 slider.oninput = function(){
-    output.innerHTML = this.value + "mts.";
+    output.innerHTML = this.value;
 }
