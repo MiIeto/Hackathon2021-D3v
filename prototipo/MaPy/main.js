fdf626c8c9333388b9e2209b41710535984a955c
Tomeitos <cgonzalezperez.cgp@gmail.com>
2021-06-26 04:07:53 -0400

    Integracion valor metros

---
+++ prototipo/MaPy/main.js
@@ -0,0 +1,8 @@
+var slider = document.getElementById("myRango");
+var output = document.getElementById("value");
+
+output.innerHTML = slider.value;
+
+slider.oninput = function(){
+    output.innerHTML = this.value + "mts.";
+}
